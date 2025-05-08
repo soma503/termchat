569de82cb743612ca6eaa08da8772ea1218346da
soma503 <julian.to2023@gmail.com>
2025-05-07 23:36:36 -0700

    added test script for client.go

---
+++ client/client_test.go
@@ -0,0 +1,20 @@
+package client_test
+
+import (
+	"github.com/soma503/termchat/client"
+	"testing"
+)
+
+func TestSendingPayload(t *testing.T) {
+
+	tt := []struct {
+		test    string
+		payload []byte
+		want    []byte
+	}{}
+	for _, tc := range tt {
+		tc.payload = nil
+
+	}
+
+}
